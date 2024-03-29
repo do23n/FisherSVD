798de0351b12277e8984fae8d5af2cbe001ca694
nhudo_umass_edu <nhudo_umass_edu@gypsum-gpu089.unity.rc.umass.edu>
2024-03-29 19:19:40 +0000

    Add bar plots for sensitivity versis mean fisher, remove line plots

--- plot_utils.py
+++ plot_utils.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 def plot_heatmap(x_values, y_values, x_label, y_label, title, heatmap_matrix, filename):
     fig, ax = plt.subplots(figsize=(10,10))
@@ -62,27 +63,36 @@ def plot_sensitivity_heatmap(sensitivity_dict, num_adj_blocks, title, filename):
                     filename=fname)
         i += cur_block_size
 
-def plot_lines(x_values, y1_values, y2_values, x_label, y1_label, y2_label, title, truncation_ratio, filename):
-    fig, ax1 = plt.subplots(figsize=(10,10))
 
-    color = 'tab:red'
+def plot_bars(x_values, y1_values, y2_values, x_label, y1_label, y2_label, title, truncation_ratio, filename):
+    data = {y1_label: y1_values, y2_label: y2_values}
+    df = pd.DataFrame(data=data, index=x_values)
+
+    width=0.2
+
+    fig, ax1 = plt.subplots(figsize=(10,10))
+    color = 'red'
     ax1.set_xlabel(x_label, fontsize=15)
     ax1.set_ylabel(y1_label, color=color, fontsize=15)
-    line1 = ax1.plot(x_values, y1_values, "-o", color=color, label='sensitivity for trunc_ratio = {}'.format(truncation_ratio))
-    ax1.tick_params(axis='y', labelcolor=color, labelsize=15)
+    df[y1_label].astype(float).plot(kind='bar', color=color, ax=ax1, width=width, position=1, label='sensitivity for trunc_ratio = {}'.format(truncation_ratio))
     ax1.tick_params(axis='x', labelsize=15)
     ax1.set_title(title, fontsize=15)
 
     ax2 = ax1.twinx() # instantiate a second axes that shares the same x-axis
-
-    color = 'tab:blue'
+    color = 'blue'
     ax2.set_ylabel(y2_label, color=color, fontsize=15) # we already handled the x-label with ax1
-    line2 = ax2.plot(x_values, y2_values, "-o", color=color, label='mean fisher info')
+    df[y2_label].astype(float).plot(kind='bar', color=color, ax=ax2, width=width, position=0, label='mean fisher info')
     ax2.tick_params(axis='y', labelcolor=color, labelsize=15)
 
-    lines = line1 + line2
-    ax1.legend(lines, [l.get_label() for l in lines], loc=1, prop={'size': 10})
+    ax1.legend(loc='upper center', bbox_to_anchor=(0.5, 1.3))
+    ax2.legend(loc='upper center', bbox_to_anchor=(0.5, 1.2))
     fig.tight_layout()
+
+    # annotate value on bar plots
+    for p in ax1.patches:
+        ax1.annotate(str(round(p.get_height(),6)), (p.get_x() * 1.005, p.get_height() * 1.005))
+    for p in ax2.patches:
+        ax2.annotate(str(round(p.get_height(),6)), (p.get_x() * 1.005, p.get_height() * 1.005))
 
     # Rotating the x-axis labels for better readability
     plt.setp(ax1.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
@@ -101,18 +111,18 @@ def plot_sensitivity_and_fisher(sensitivity_dict, fisher_dict, truncation_ratio,
 
         layer_idx = int(layer_names[i].split(".")[2]) if "." in cur_layer else -1
         if  "." not in cur_layer:
-            fname = filename + f"/adj_blocks_{num_adj_blocks}/layer-{cur_layer}"
+            fname = filename + f"/ratio_{int(truncation_ratio*10)}/adj_blocks_{num_adj_blocks}/layer-{cur_layer}"
         elif num_adj_blocks == 1:
-            fname = filename + f"/adj_blocks_{num_adj_blocks}/layer-{layer_idx}"
+            fname = filename + f"/ratio_{int(truncation_ratio*10)}/adj_blocks_{num_adj_blocks}/layer-{layer_idx}"
         elif num_adj_blocks > 1:
-            fname = filename + f"/adj_blocks_{num_adj_blocks}/layer-{layer_idx}-{layer_idx - (num_adj_blocks - 1)}"
+            fname = filename + f"/ratio_{int(truncation_ratio*10)}/adj_blocks_{num_adj_blocks}/layer-{layer_idx}-{layer_idx - (num_adj_blocks - 1)}"
 
         end_idx = i + cur_block_size + next_block_size*(num_adj_blocks-1)
 
         perplexity_values = [sensitivity_dict[layer_name][truncation_ratio] for layer_name in layer_names][i:end_idx]
         fisher_values = [fisher_dict[name] for name in layer_names[i:end_idx]]
         
-        plot_lines(x_values=layer_names[i:end_idx], 
+        plot_bars(x_values=layer_names[i:end_idx], 
                    y1_values=perplexity_values, 
                    y2_values=fisher_values, 
                    x_label="layer", 
